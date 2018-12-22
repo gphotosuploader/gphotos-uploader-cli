c23b833a42c9c32fb3a6ed07fc573d60e9232fb9
pacoorozco <paco@pacoorozco.info>
2018-12-22 11:32:44 +0100

    Remove unused code
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- config/config.go
+++ config/config.go
@@ -14,44 +14,35 @@ import (
 	"github.com/client9/xson/hjson"
 )
 
+const exampleConfig = `
+{
+  APIAppCredentials: {
+    ClientID:     "20637643488-1hvg8ev08r4tc16ca7j9oj3686lcf0el.apps.googleusercontent.com",
+    ClientSecret: "0JyfLYw0kyDcJO-pGg5-rW_P",
+  }
+  jobs: [
+    {
+      account: youremail@gmail.com
+      sourceFolder: ~/folder/to/upload
+      makeAlbums: {
+        enabled: true
+        use: folderNames
+      }
+      deleteAfterUpload: true
+    }
+  ]
+}
+`
+
 type APIAppCredentials struct {
 	ClientID     string
 	ClientSecret string
 }
 
-var (
-	// consts
-	DEFAULT_API_APP_CREDENTIALS = APIAppCredentials{
-		ClientID:     "20637643488-1hvg8ev08r4tc16ca7j9oj3686lcf0el.apps.googleusercontent.com",
-		ClientSecret: "0JyfLYw0kyDcJO-pGg5-rW_P",
-	}
-
-	// vars
-	Cfg *Config
-)
-
-type Config struct {
-	APIAppCredentials *APIAppCredentials
-	Jobs              []FolderUploadJob
-}
-
-func (c *Config) Process() {
-	if c.APIAppCredentials == nil {
-		c.APIAppCredentials = &DEFAULT_API_APP_CREDENTIALS
-	}
-}
-
-func OAuthConfig() *oauth2.Config {
-	if Cfg.APIAppCredentials == nil {
-		log.Fatal(stacktrace.NewError("APIAppCredentials can't be nil"))
-	}
-	return gphotos.NewOAuthConfig(gphotos.APIAppCredentials(*Cfg.APIAppCredentials))
-}
-
 type FolderUploadJob struct {
-	Account      string
-	SourceFolder string
-	MakeAlbums   MakeAlbums
+	Account           string
+	SourceFolder      string
+	MakeAlbums        MakeAlbums
 	DeleteAfterUpload bool
 }
 
@@ -60,38 +51,21 @@ type MakeAlbums struct {
 	Use     string
 }
 
-//func LoadConfigFile() *Config {
-//	Cfg = loadConfigFile()
-//	Cfg.Process()
-//	return Cfg
-//}
+type Config struct {
+	APIAppCredentials *APIAppCredentials
+	Jobs              []FolderUploadJob
+}
 
-//var noConfigFoundMessage = color.CyanString(`
-//No config file found at ~/.config/gphotos-uploader-cli/config.hjson
-//Will now copy an example config file.
-//Edit it by running:
-//
-//	nano ~/.config/gphotos-uploader-cli/config.hjson
-//
-//`)
+var (
+	Cfg *Config
+)
 
-//func loadConfigFile() (*Config, error) {
-//	configPathAbsolute, err := cp.AbsolutePath(CONFIGPATH)
-//	if err != nil {
-//		log.Fatal(err)
-//	}
-//
-//	// if no config file copy the default example and exit
-//	if !fileshandling.IsFile(configPathAbsolute) {
-//		fmt.Println(noConfigFoundMessage)
-//		if err := InitConfigFile(); err != nil {
-//			log.Fatal(stacktrace.Propagate(err, "failed initializing config file"))
-//		}
-//		os.Exit(0)
-//	}
-//
-//	return LoadFromFile(configPathAbsolute)
-//}
+func OAuthConfig() *oauth2.Config {
+	if Cfg.APIAppCredentials == nil {
+		log.Fatal(stacktrace.NewError("APIAppCredentials can't be nil"))
+	}
+	return gphotos.NewOAuthConfig(gphotos.APIAppCredentials(*Cfg.APIAppCredentials))
+}
 
 // LoadConfigFile reads HJSON file (absolute path) and decodes its configuration
 func LoadConfigFile(p string) (*Config, error) {
@@ -145,23 +119,3 @@ func InitConfigFile(p string) error {
 
 	return fh.Sync()
 }
-
-const exampleConfig = `
-{
-  APIAppCredentials: {
-    ClientID:     "20637643488-1hvg8ev08r4tc16ca7j9oj3686lcf0el.apps.googleusercontent.com",
-    ClientSecret: "0JyfLYw0kyDcJO-pGg5-rW_P",
-  }
-  jobs: [
-    {
-      account: youremail@gmail.com
-      sourceFolder: ~/folder/to/upload
-      makeAlbums: {
-        enabled: true
-        use: folderNames
-      }
-      deleteAfterUpload: true
-    }
-  ]
-}
-`
