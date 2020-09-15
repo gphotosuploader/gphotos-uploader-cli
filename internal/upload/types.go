e619c982cdc2b4a55e6bf225444c2a7bfd5fb266
pacoorozco <paco@pacoorozco.info>
2020-09-15 10:12:04 +0200

    Recover code deleted by mistake
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- internal/upload/types.go
+++ internal/upload/types.go
@@ -1,5 +1,11 @@
 package upload
 
+// FileTracker represents a service to track already uploaded files.
+type FileTracker interface {
+	CacheAsAlreadyUploaded(filePath string) error
+	IsAlreadyUploaded(filePath string) (bool, error)
+	RemoveAsAlreadyUploaded(filePath string) error
+}
 
 // UploadFolderJob represents a job to upload all photos from the specified folder
 type UploadFolderJob struct {
