88e63add79e7d9b7ff5666dbab2008a11df6ce88
Matthijs Langenberg <992+mlangenberg@users.noreply.github.com>
2021-09-11 12:15:05 +0200

    Fix and add test cases for filetracker/entity

--- internal/datastore/filetracker/entity_test.go
+++ internal/datastore/filetracker/entity_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gphotosuploader/gphotos-uploader-cli/internal/datastore/filetracker"
+	"time"
 )
 
 func TestTrackedFile_Hash(t *testing.T) {
@@ -19,7 +20,49 @@ func TestTrackedFile_Hash(t *testing.T) {
 	for _, tc := range testCases {
 		t.Run(tc.name, func(t *testing.T) {
 			f := filetracker.NewTrackedFile(tc.input)
-			got := f.Hash()
+			got := f.Hash
+			if tc.want != got {
+				t.Errorf("want: %s, got: %s", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestTrackedFile_ModTime(t *testing.T) {
+	testCases := []struct {
+		name string
+		input string
+		want time.Time
+	}{
+		{"Should return zero time value", "123456789", time.Time{}},
+		{"Should return time value", "1631350013816466000|123456789", time.Unix(0, 1631350013816466000)},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			f := filetracker.NewTrackedFile(tc.input)
+			got := f.ModTime
+			if tc.want != got {
+				t.Errorf("want: %s, got: %s", tc.want, got)
+			}
+		})
+	}
+}
+
+func TestTrackedFile_String(t *testing.T) {
+	testCases := []struct {
+		name string
+		input string
+		want string
+	}{
+		{"Should return the hash", "123456789", "123456789"},
+		{"Should return mtime and hash", "1631350013816466000|123456789", "1631350013816466000|123456789"},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			f := filetracker.NewTrackedFile(tc.input)
+			got := f.String()
 			if tc.want != got {
 				t.Errorf("want: %s, got: %s", tc.want, got)
 			}
