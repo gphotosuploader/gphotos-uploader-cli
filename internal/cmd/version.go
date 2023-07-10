038a0e48cadf49057bc41efd0ecf324701b7f728
pacoorozco <paco@pacoorozco.info>
2023-07-10 15:57:34 +0200

    Fix typos
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- internal/cmd/version.go
+++ internal/cmd/version.go
@@ -8,15 +8,15 @@ import (
 //
 // This is the fallback data used when version information from git is not
 // provided via go ldflags. It provides an approximation of the application
-// version for ad-hoc builds (e.g. `go build`) that cannot get the version
+// version for adhoc builds (e.g. `go build`) that cannot get the version
 // information from git
 //
 // If you are looking at these fields in the git tree, they look strange. They
 // are modified on the fly by the build process.
 //
 // We use semantic version (see https://semver.org/ for more information). When
-// releasing a new version, this file is updated by Makefile to reflect the new
-// version, a git annotated tag is used to set this version
+// releasing a new version, Makefile updates this file to reflect the new
+// version; a git-annotated tag is used to set this version
 var (
 	version = "v0.0.0" // git tag, output of $(git describe --tags --always --dirty)
 )
@@ -36,5 +36,5 @@ func NewVersionCmd() *cobra.Command {
 }
 
 func (cmd *VersionCmd) Run(command *cobra.Command, args []string) {
-	command.Printf("gphotos-uploader-cli %s\n", version)
+	command.Printf("gphotos-cli %s\n", version)
 }
