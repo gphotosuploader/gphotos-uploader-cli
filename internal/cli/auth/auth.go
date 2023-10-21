c359ca216dd84f2c0fc3a2722b6d6b2d83cfd322
pacoorozco <paco@pacoorozco.info>
2023-10-21 12:14:14 +0200

    Add a flag to the auth command to set the local port
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- internal/cli/auth/auth.go
+++ internal/cli/auth/auth.go
@@ -12,6 +12,9 @@ import (
 // AuthCmd holds the required data for the init cmd
 type AuthCmd struct {
 	*flags.GlobalFlags
+
+	// command flags
+	Port int
 }
 
 func NewCommand(globalFlags *flags.GlobalFlags) *cobra.Command {
@@ -24,6 +27,8 @@ func NewCommand(globalFlags *flags.GlobalFlags) *cobra.Command {
 		Args:  cobra.NoArgs,
 		RunE:  cmd.Run,
 	}
+
+	authCmd.Flags().IntVarP(&cmd.Port, "port", "p", 0, "port on which the auth server will listen (default 0)")
 
 	return authCmd
 }
@@ -38,7 +43,7 @@ func (cmd *AuthCmd) Run(cobraCmd *cobra.Command, args []string) error {
 		_ = cli.Stop()
 	}()
 
-	_, err = cli.AuthenticateFromWeb(ctx)
+	_, err = cli.AuthenticateFromWeb(ctx, cmd.Port)
 	if err == nil {
 		cli.Logger.Donef("Successful authentication for account '%s'", cli.Config.Account)
 	}
