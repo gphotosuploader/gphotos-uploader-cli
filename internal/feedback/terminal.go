3120d10eb722508d5e07570a7049a2b945e9748e
pacoorozco <paco@pacoorozco.info>
2024-02-25 18:14:35 +0100

    Refactor terminal.go functions
    Signed-off-by: pacoorozco <paco@pacoorozco.info>

--- internal/feedback/terminal.go
+++ internal/feedback/terminal.go
@@ -19,43 +19,65 @@ func InputUserField(prompt string, secret bool) (string, error) {
 		return "", errors.New("user input not supported in non interactive mode")
 	}
 
-	fmt.Fprintf(stdOut, "%s: ", prompt)
+	printPrompt(prompt)
 
 	if secret {
-		// Read and return a password (no character echoed on terminal)
-		value, err := term.ReadPassword(int(os.Stdin.Fd()))
-		fmt.Fprintln(stdOut)
-		return string(value), err
+		return readPassword()
 	}
 
-	// Read and return an input line
+	return readInputLine()
+}
+
+// printPrompt prints the prompt to the user.
+func printPrompt(prompt string) {
+	fmt.Fprintf(stdOut, "%s: ", prompt)
+}
+
+// readPassword reads and returns a password from the user.
+func readPassword() (string, error) {
+	value, err := term.ReadPassword(int(os.Stdin.Fd()))
+	fmt.Fprintln(stdOut)
+	return string(value), err
+}
+
+// readInputLine reads and returns an input line from the user.
+func readInputLine() (string, error) {
 	sc := bufio.NewScanner(os.Stdin)
 	sc.Scan()
 	return sc.Text(), sc.Err()
 }
 
 func YesNoPrompt(prompt string, def bool) (bool, error) {
-	choices := "Y/n"
-	if !def {
-		choices = "y/N"
-	}
-
+	choices := getChoices(def)
 	prompt = fmt.Sprintf("%s (%s)", prompt, choices)
 
 	for {
 		s, err := InputUserField(prompt, false)
-		if err != nil {
+		if err != nil || s == "" {
 			return def, err
 		}
-		if s == "" {
-			return def, nil
-		}
+
 		s = strings.ToLower(s)
-		if s == "y" || s == "yes" {
+		if isYes(s) {
 			return true, nil
 		}
-		if s == "n" || s == "no" {
+		if isNo(s) {
 			return false, nil
 		}
 	}
+}
+
+func getChoices(def bool) string {
+	if !def {
+		return "y/N"
+	}
+	return "Y/n"
+}
+
+func isYes(s string) bool {
+	return s == "y" || s == "yes"
+}
+
+func isNo(s string) bool {
+	return s == "n" || s == "no"
 }
